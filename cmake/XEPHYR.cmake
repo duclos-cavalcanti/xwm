6841d41c1d4349b97708c2652a6f1b628f543f86
Daniel Duclos-Cavalcanti <me@duclos.dev>
2023-06-13 10:10:10 -0300

    Added cmake file to find Xephyr executable

---
+++ cmake/XEPHYR.cmake
@@ -0,0 +1,13 @@
+function (Find_Xephyr)
+    execute_process(COMMAND whereis Xephyr 
+                    OUTPUT_QUIET
+                    RESULT_VARIABLE RUNCODE)
+
+    if (${RUNCODE} EQUAL 0)
+        set(XEPHYR_FOUND 1 PARENT_SCOPE)
+        message(STATUS "Xephyr executable found")
+    else()
+        set(XEPHYR_FOUND 0 PARENT_SCOPE)
+    endif()
+endfunction()
+
