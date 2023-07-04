3ea0606ca1f64bfa902d41e45cbd98e92f701158
Daniel Duclos-Cavalcanti <me@duclos.dev>
2023-07-04 13:44:58 -0300

    Added barebones to testing with check

---
+++ test/src/test.c
@@ -0,0 +1,23 @@
+#include "test.h"
+#include "main.h"
+
+window_manager_t *wm = { 0 };
+
+START_TEST(linked_list)
+{
+
+}
+END_TEST
+
+Suite *suite()
+{
+
+}
+
+int main(void) {
+    Suite *s;
+    SRunner *sr;
+
+    s = suite();
+    
+}
